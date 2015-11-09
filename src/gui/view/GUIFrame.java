165177809c15a5d28b6a863f21c41d1e425a0a58
Alejandro <AlexMJHS@gmail.com>
2015-11-09 09:30:46 -0700

    Added setTitle

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -24,6 +24,7 @@ public class GUIFrame extends JFrame
 	{
 		this.setContentPane(basePanel);  //Must be first line of setupFrame! Installs he panel in the frame.
 		this.setSize(400,400);           //Find a good size for the app.
+		this.setTitle("Simple App");
 		this.setResizable(false);        //Advisable not required.
 		this.setVisible(true);           //Must be last line of setupFrame!
 	}
